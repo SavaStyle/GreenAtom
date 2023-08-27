98e3138b0647907000396b8203aa9cec78a70645
SavaStyle <sava.90@inbox.ru>
2023-08-27 15:07:57 +0300

    initial GA

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -10,32 +8,32 @@ public class Main {
 
     String SQL = new String(
             "SELECT surname " +
-            "FROM users  " +       // в условии нет названия таблицы
-            "ORDER BY experience DESC" +
-            "LIMIT 1 OFFSET 1;" );
+                    "FROM users  " +       // в условии нет названия таблицы
+                    "ORDER BY experience DESC" +
+                    "LIMIT 1 OFFSET 1;");
 
 //  Задача 2 замена значений переменных без использования третьей
 
-    public void swap (int a, int b) {
+    public void swap(int a, int b) {
         a = a + b;
         b = a - b;
         a = a - b;
     }
 
 
-// Задача 3.1 инверсия односвязного списка
-    public void inversion () {
+    // Задача 3.1 инверсия односвязного списка
+    public void inversion() {
 
-        Node n4 = new Node("4",null);
-        Node n3 = new Node("3",n4);
-        Node n2 = new Node("2",n3);
-        Node n1 = new Node("1",n2);
+        Node n4 = new Node("4", null);
+        Node n3 = new Node("3", n4);
+        Node n2 = new Node("2", n3);
+        Node n1 = new Node("1", n2);
 
         Node head = n1;
         Node previous = null;
         Node next = head.next;
 
-        while(next != null) {
+        while (next != null) {
             head.next = previous;
             previous = head;
             head = next;
@@ -46,13 +44,13 @@ public class Main {
 
 // Задача 3.2 проверка полиндрома
 
-    public boolean isPalindrom (String string) {
+    public boolean isPalindrom(String string) {
         StringBuilder builder = new StringBuilder(string);
         builder.reverse();
         String reverse = builder.toString();
-        if(string.equals(reverse)){
-           return true;
-        }else{
+        if (string.equals(reverse)) {
+            return true;
+        } else {
             return false;
         }
     }
